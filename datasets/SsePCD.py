2d00f0aa5e25eb815d807e6df2495cfea4ac1ceb
Dashark <dasha.hu@gmail.com>
2021-12-02 21:45:41 +0800

    标注字段名称

--- datasets/SsePCD.py
+++ datasets/SsePCD.py
@@ -625,7 +625,7 @@ class SsePCDDataset(PointCloudDataset):
                 # Save as ply
                 write_ply(ply_cloud_file,
                           (cloud_points, cloud_colors, cloud_classes),
-                          ['x', 'y', 'z', 'red', 'green', 'blue', 'class'])
+                          ['x', 'y', 'z', 'red', 'green', 'blue', 'label'])
         # self.cloud_names.append('output')
         self.all_splits = [i for i in range(len(self.cloud_names))]
         self.validation_split = [0] #[i for i in range(len(self.cloud_names)) if i % 10 == 0]
